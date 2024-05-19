4633e5a2704303279b222714e47bf9164fa43d4f
Hexi1997 <18715156450@163.com>
2024-05-19 22:24:56 +0800

    feat: add sub page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export const runtime = 'edge'
+
+export default async function NotFound() {
+    return (
+        <main className="min-h-screen flex items-center justify-center">Not founded</main>
+    )
+}
---
+++ app/sub/page.tsx
@@ -0,0 +1,8 @@
+export default function SubPage() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        sub page
+      </main>
+    );
+  }
+  
